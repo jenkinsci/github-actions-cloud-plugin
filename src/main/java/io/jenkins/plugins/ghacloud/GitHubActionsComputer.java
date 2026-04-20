43855cd922f41bbb387de7a436bffcf4cd4b1a7d
George Adams <georgeadams1995@gmail.com>
2026-04-20 11:54:00 +0100

    Integrate with cloud-stats plugin for provisioning activity tracking
    Cloud Statistics (Health) always showed 0 for all phases because the
    plugin had no integration with the cloud-stats plugin.
    
    Changes:
    - Add cloud-stats dependency to pom.xml
    - Implement TrackedItem on GitHubActionsAgent and GitHubActionsComputer
      so cloud-stats can track agents through provisioning phases
    - Create ProvisioningActivity.Id during provisioning and pass it through
      to the agent, enabling automatic tracking of Provisioning, Launching,
      and Operating phases

--- src/main/java/io/jenkins/plugins/ghacloud/GitHubActionsComputer.java
+++ src/main/java/io/jenkins/plugins/ghacloud/GitHubActionsComputer.java
@@ -1,10 +1,18 @@
 package io.jenkins.plugins.ghacloud;
 
 import hudson.slaves.AbstractCloudComputer;
+import org.jenkinsci.plugins.cloudstats.ProvisioningActivity;
+import org.jenkinsci.plugins.cloudstats.TrackedItem;
 
-public class GitHubActionsComputer extends AbstractCloudComputer<GitHubActionsAgent> {
+public class GitHubActionsComputer extends AbstractCloudComputer<GitHubActionsAgent> implements TrackedItem {
 
     public GitHubActionsComputer(GitHubActionsAgent agent) {
         super(agent);
+    }
+
+    @Override
+    public ProvisioningActivity.Id getId() {
+        GitHubActionsAgent node = getNode();
+        return node != null ? node.getId() : null;
     }
 }
